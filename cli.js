061c8002f325cb38cb0118c84ba3efeef58f8a18
itsara <khajer@yahoo.com>
2016-07-09 16:49:58 +0700

    receive correct parameter

--- cli.js
+++ cli.js
@@ -9,9 +9,14 @@ var main = function(){
 	console.log(process.argv);
 	program
 		.version('0.0.1')
-		.command('create-app <projectName>', 'create project ')
-		.action(function (projectName){
-			console.log('project name:'+projectName);
+		.command('<func> <projectName>', 'create project ')
+		.action(function (func, projectName){
+			if(func=="create-app"){
+				console.log('project name:'+projectName);
+			}else{
+
+			}
+			
 		});
 	program.parse(process.argv);
 
