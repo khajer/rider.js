ddc14a48261b5061c9bcca80157ef0725a09c777
itsara <khajer@yahoo.com>
2016-07-09 17:44:11 +0700

    remove commander

--- cli.js
+++ cli.js
@@ -1,21 +1,11 @@
 #!/usr/bin/env node
 
-var program = require('commander')
 var currPath = process.cwd();
 
 
 var main = function(){
-	console.log("-- rider generator--");
-	program
-		.version('0.0.1')
-		.command('<func> <projectName>')
-		.option("create-app <projectName>", "create new project ")
-		.action(function (func, projectName){
-			console.log("function : "+func)
-			console.log('create new project: "'+projectName+'"');
-			
-		});
-	program.parse(process.argv);
+	console.log(">> rider generator ");
+
 
 }
 
