732890136da11a0b9fbedef6e84e5a111c9d6c7e
itsara <khajer@yahoo.com>
2016-07-10 18:01:13 +0700

    update create project file

--- cli.js
+++ cli.js
@@ -16,8 +16,8 @@ var printHelp = function(){
 		+"\n\n"
 		+h+"Commands: "+"\n\n"
 		+t+"create-app [appName]"+"\t"+"create new app"+"\n"
-		+t+"create-model [modelName]"+"\t"+"create new Data Model"+"\n"
-		+t+"gen-model [modelName]"+"\t"+"generate controller Data Model"+"\n"
+		// +t+"create-model [modelName]"+"\t"+"create new Data Model"+"\n"
+		// +t+"gen-model [modelName]"+"\t"+"generate controller Data Model"+"\n"
 		+"";
 	console.log(txtHelp);
 
@@ -27,7 +27,6 @@ var parseParam = function(params, cb){
 	if(params.length < 3){
 		return cb(true, null, null);
 	}
-
 	cb(null, params[2], params[3]);
 }	
 
@@ -42,28 +41,87 @@ var checkCommand = function(cmd){
 	return !foundCmd;
 }
 
+var copyFile = function(source, target, cb){
+	var cbCalled = false;
+	var rd = fs.createReadStream(source);
+	rd.on("error", function(err) { done(err); });
+	
+	var wr = fs.createWriteStream(target);
+	wr.on("error", function(err) { done(err); });
+	wr.on("close", function(ex) { done(); });
+	rd.pipe(wr);
+	function done(err) {
+		if (!cbCalled) {
+			cb(err);
+			cbCalled = true;
+		}
+	}
+}
+
 var copyPrototype = function(projectName, cb){
 	console.log('> initial prototype');
 	console.log('> read prototype file');
 
-	var p = __dirname+'/prototype';
-	var cmdPath = process.cwd();
-	
-	console.log(">    libPath : "+p);
-	console.log(">    currPath : "+cmdPath)
+	fs.realpath(__dirname, function(err, p){
+		if(err){
+			console.log('error');
+			return;
+		}
+		console.log(p);
 
-	//create folder project
-	console.log('>    create folder project: '+projectName);
-	var targetPath = cmdPath+'/'+projectName;
-	fs.mkdir(targetPath, function(e){
-		console.log('>    create folder completed');
-		cb(false);
-	});	
+	});
+	var p = __dirname + '/prototype';
+	var cmdPath = process.cwd();
+	// console.log("lib path: "+p);
+	// console.log("command path: "+cmdPath);
+
+	//create folder project name
+	console.log('initial project : '+projectName);
+	var targetPath = cmdPath+"/"+projectName;
+
+	fs.mkdir(targetPath,function(e){
+		if(!e || (e && e.code === 'EEXIST')){
+			//copy index.js (main file)
+			var srcFile = p+'/index.js';
+			var desFile = targetPath+'/index.js';
+			// console.log('copy file from: '+srcFile+', to:'+desFile);
+			copyFile(srcFile, desFile, function(err){
+				if(err){
+					console.log(err);
+					cb(true);
+				}
+
+				//config project file (package.json)
+				var packageFile = p+'/package.json';
+				fs.readFile(packageFile, 'utf8', function(err, data) {
+					if (err){
+						console.log(err);
+						cb(true);
+						return;
+					}
+					var objFile = JSON.parse(data);
+					objFile.name = projectName;
+					var txt	= JSON.stringify(objFile, null, 2);
+					var packageFileTar = targetPath+'/package.json';
+					fs.writeFile(packageFileTar, txt, function(err) {
+						if(err) {
+							return console.log(err);
+						}
+						cb(false);	
+					});
+				});				
+			});			
+		}else{
+			console.log(e);
+			cb(true);
+		}
+	});
+
 }
 var runCommand = function(opt, param, cb){
 	if(opt == "create-app"){
 		copyPrototype(param, function(err){
-			cb();
+			cb();		
 		});
 	}	
 }
@@ -88,9 +146,8 @@ var main = function(){
 			process.exit(0);	
 		});
 	});
-
 }
-
+// -- call main function --
 main();
 
 
