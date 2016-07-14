d25a4e9eb103458cb8869f6d8ccc9dee51d39bad
itsara <khajer@yahoo.com>
2016-07-14 15:03:23 +0700

    increase load controller file & route

--- prototype/index.js
+++ prototype/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('it work.');
 });
+
+// load file controllers
+var contPath = './app/controllers';
+var contList = fs.readdirSync(contPath);
+contList.forEach(function(item){
+	var fileRequire = "./app/controllers/"+item;
+	var filetype = item.substring( item.length -3 , item.length);
+	if(filetype==".js"){
+		require(fileRequire).init(app);		
+	}
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
