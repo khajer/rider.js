1ad62539b1976c39a75a0f40a36dac36f4ee1826
itsara <khajer@yahoo.com>
2016-07-16 21:58:23 +0700

    change help

--- cli.js
+++ cli.js
@@ -6,24 +6,64 @@ const path = require('path');
 var createApp = require('./create-app.js');
 var createController = require('./create-controller.js');
 var createModel = require('./create-model.js');
+var genModel = require('./gen-model.js');
 
 var commandList = [
 	"create-app",
 	"create-controller",
 	"create-model",
+	"gen-model",
+	"gen-rest-model"
 ];
+
+var cmdList = [{
+		name:"create-app", 
+		params:'appName',
+		desc: 'create new app'
+	},{
+		name:"create-controller", 
+		params:'controllerName',
+		desc: 'create controller (route)'
+	},{
+		name:"create-model", 
+		params:'modelName',
+		desc: 'create new Data Model (database)'
+	},{
+		name:"gen-model", 
+		params:'modelName',
+		desc: 'auto generate controller with Data model'
+	},{
+		name:"gen-rest-model", 
+		params:'modelName',
+		desc: 'auto generate controller REST API with Data model'
+	}
+]
 
 var printHelp = function(){
 	var txtHelp = "";
 	var h = '  ';
 	var t = '    ';
-	txtHelp += "\n"+h+"Usage : rider [command]"
-		+"\n\n"
-		+h+"Commands: "+"\n\n"
-		+t+"create-app [appName]"+"\t\t"+"create new app"+"\n"
-		+t+"gen-controller [controllerName]"+"\t"+"create controller "+"\n"
-		+t+"create-model [modelName]"+"\t\t"+"create new Data Model"+"\n"
-		+"";
+
+	txtHelp += "\n"+h+"Usage : rider [command] \n";
+
+	txtHelp += "\n"+h+"Command List : \n";
+	var maxText = 0;
+	cmdList.forEach(function(item){
+		if(maxText < item.name.length+item.params.length) 
+			maxText = item.name.length+item.params.length;
+	});
+	var maxT = Math.ceil(maxText/4);
+	cmdList.forEach(function(item){
+		var x = (item.name.length+item.params.length)/4;
+		x = Math.ceil(x);
+		var space = '\t';
+		for (var i = 0; i < maxT-x-1; i++) {
+			space += '\t'; 
+		}
+		var txt = '\t'+item.name +" [" +item.params+"] " + space +item.desc+"\n";
+		txtHelp +=txt;
+	});		
+	txtHelp	+"\n";
 	console.log(txtHelp);
 	process.exit(0);
 
@@ -37,8 +77,8 @@ var parseParam = function(params, cb){
 
 var checkCommand = function(cmd){
 	var foundCmd = false;
-	commandList.forEach(function(command){
-		if(cmd == command){
+	cmdList.forEach(function(item){
+		if(item.name == cmd){
 			foundCmd = true;
 			return;
 		}
@@ -77,7 +117,19 @@ var runCommand = function(opt, param, cb){
 			console.log("create controller '"+param+ "' completed");
 			cb();
 		});
-	}	
+	}else if(opt == "gen-model"){
+		console.log("generate controller with model: " + param)
+		genModel.generateControllerModel(param, function(err){
+
+			if(err){
+				console.log("create controller '"+param+ "' fails");
+				cb();
+			}
+			console.log("generate model to controller '"+param+ "' completed");
+			cb();
+		});
+
+	}
 }
 
 var main = function(){
