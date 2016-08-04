95a3032a2e789d4e9db1447ea5eacba445be5b71
itsara <khajer@yahoo.com>
2016-08-04 23:28:24 +0700

    precode

--- libs/create-login-model.js
+++ libs/create-login-model.js
@@ -1,3 +1,9 @@
+var prompt = require('prompt');
+var colors = require("colors/safe")
+
+prompt.start();
+prompt.message = colors.rainbow(" >> ");
+
 module.exports = {
 	init: function(cb){
 		cb();
